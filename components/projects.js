ab794f1bb29627ed4058398ef0a2992c4bcf0192
sushmeet sunger <ssunger@shutterstock.com>
2021-11-09 09:47:38 -0500

    center projects section on large screens

--- components/projects.js
+++ components/projects.js
@@ -6,31 +6,41 @@ export const Projects = ({
   technologies = ["Html", "CSS", "SVG"],
 }) => {
   return (
-    <section id="projects" className={styles.projects}>
-      <h2 className={styles.projectsMainHeader}>Projects I'm proud of</h2>
-      <h4>{projectTitle}</h4>
-      <h3 className={styles.projectName}>{projectName}</h3>
-      <p className={styles.projectParagraph}>
-        Description of the project. This should be fairly concise while also
-        describing the key components that you developed or worked on. It can be
-        as long as you need it to be but should at least be a few sentences
-        long. Be sure to include specific links anywhere in the description. A
-        link looks like{" "}
-        <a href="https://frontendmasters.github.io/grid-flexbox-v2/">this</a>,
-        and multiple links look <a href="#">like this</a> and{" "}
-        <a href="#">like this</a>.
-      </p>
-      <h4 className={styles.projectTechnologiesUsedHeading}>Technologies used include:</h4>
-      <ul className={styles.techList}>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>SVG</li>
-      </ul>
-      <img
-        className={styles.projectImage}
-        src="https://assets.codepen.io/296057/fem-gettingstartedcss-ch5-1.png"
-        alt="lates wall of wonder"
-      />
-    </section>
+    <div className={styles.projectsSectionColor}>
+      <section id="projects" className={styles.projects}>
+        <h2 className={styles.projectsMainHeader}>Projects I'm proud of</h2>
+        <article className={styles.article}>
+          <div className={styles.text}>
+            <h4>{projectTitle}</h4>
+            <h3 className={styles.projectName}>{projectName}</h3>
+            <p className={styles.projectParagraph}>
+              Description of the project. This should be fairly concise while
+              also describing the key components that you developed or worked
+              on. It can be as long as you need it to be but should at least be
+              a few sentences long. Be sure to include specific links anywhere
+              in the description. A link looks like{" "}
+              <a href="https://frontendmasters.github.io/grid-flexbox-v2/">
+                this
+              </a>
+              , and multiple links look <a href="#">like this</a> and{" "}
+              <a href="#">like this</a>.
+            </p>
+            <h4 className={styles.projectTechnologiesUsedHeading}>
+              Technologies used include:
+            </h4>
+            <ul className={styles.techList}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>SVG</li>
+            </ul>
+          </div>
+          <img
+            className={styles.projectImage}
+            src="https://assets.codepen.io/296057/fem-gettingstartedcss-ch5-1.png"
+            alt="lates wall of wonder"
+          />
+        </article>
+      </section>
+    </div>
   );
 };
